20917a4f6dbb747f599b975158111dbffb8c23e9
takfarinassaber <takfarinassaber@outlook.com>
2023-10-12 16:04:28 +0100

    Update springboot java version

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,25 +2,34 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
 public class DemoApplication {
 
-	@RequestMapping("/")
-	String home() {
-		return "Hello World!";
-	}
-
-	@RequestMapping("/home")
-	String home2() {
-		return "Hello World!";
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+}
+
+@Controller
+class TomcatController {
+
+	@GetMapping("/")
+	public String index() {
+		return "index.html";
+	}
+}
+
+@Controller
+class HelloController {
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello there !";
+	}
 }
