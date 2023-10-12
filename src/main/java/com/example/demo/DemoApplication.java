b337034ba56c717c0b0122faff094fc16b5b62a5
takfarinassaber <takfarinassaber@outlook.com>
2023-10-12 13:58:24 +0100

    Initial url

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -13,7 +13,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-	@GetMapping("/")
+	@GetMapping("/hello")
 	public String index() {
 		return "Greetings from Spring Boot!";
 	}
