53e02bc544aae299d92d17c9ecaef2493dc5a744
takfarinassaber <takfarinassaber@outlook.com>
2023-10-12 14:12:13 +0100

    Initial url

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,8 +3,14 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@RestController
 @SpringBootApplication
 public class DemoApplication {
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
