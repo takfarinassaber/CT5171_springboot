0fd7623e360430c51ab88ea788f953d8b830e9c2
takfarinassaber <takfarinassaber@outlook.com>
2023-10-12 13:15:07 +0100

    Initial url

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,25 +1,14 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
 @RestController
-public class DemoApplication  extends SpringBootServletInitializer {
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(DemoApplication.class);
-	}
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+public class DemoApplication {
+
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
 	}
 
-	@RequestMapping(value = "/")
-	public String hello() {
-		return "Hello World from Tomcat";
-	}
 }
