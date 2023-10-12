177a7ec5bd108e28d0e5009cf8bd811876203317
takfarinassaber <takfarinassaber@outlook.com>
2023-10-12 15:51:48 +0100

    Update springboot java version

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,15 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
+@SpringBootApplication
 public class DemoApplication {
+
 	@RequestMapping("/")
 	String home() {
 		return "Hello World!";
 	}
 
-	@RequestMapping("/hello")
+	@RequestMapping("/home")
 	String home2() {
 		return "Hello World!";
 	}
